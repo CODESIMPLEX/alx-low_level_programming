487989ce447de512e326a931acfe72282ca4249f
Chukwudi Okpala <echezonaokpala322@gmail.com>
2022-07-01 08:24:59 -0700

    done just now

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
 #include <unistd.h>
 
 
 /**
-* main -prints exactly "and that piece of art is useful"
-* - Dora Korpar, 2015-10-19" ,
-* followed by a new line, to the standard error.
-* Return; Always 0 (success)
+*main -prints a text
+*Return: Always 1 (success)
 */
 int main(void)
 {
