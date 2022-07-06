0d16d98f2d21e67d4658ab9fe6344871d3a03e41
Chukwudi Okpala <echezonaokpala322@gmail.com>
2022-07-06 14:53:15 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_alphabet -> prints the lowercase alphabets
+*/
+void print_alphabet(void);
+{
+int j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchchar(j);
+}
+_putchar('\n');
+}
