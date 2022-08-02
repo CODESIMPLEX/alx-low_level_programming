b62f8f11f919fb8d608b6c8b675a594441084eb4
Chukwudi Okpala <echezonaokpala322@gmail.com>
2022-08-02 11:14:27 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,55 +1,22 @@
-#ifndef _HEADER_
-#define _HEADER_
-
+#ifndef DOG
+#define DOG
+int _putchar(char c);
 /**
-  * struct dog - Dog attributes
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
-  *
-  * Description: The attributes of a dog.
-  */
+ * struct dog - dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - Typedef for dog structure
- */
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif#ifndef _HEADER_
-#define _HEADER_
-
-/**
-  * struct dog - Dog attributes
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
-  *
-  * Description: The attributes of a dog.
-  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-/**
- * dog_t - Typedef for dog structure
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+int _strlen(char *s);
 #endif
